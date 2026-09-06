3e1c1d817728449904cfd7542ee541837295ad48
cristhianlfm3193 <cfm3193@gmail.com>
2026-09-06 10:38:16 -0500

    chore: add allowed dev origin and update image remote pattern configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.1.6'],
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
